44053565ece10e9f3e2b539877acc9e5675f16cd
Alex Frame <alex.frame@kasada.io>
2020-05-28 10:31:24 +1000

    feat: add cors
    - add cors so netlify frontend can contact the backend

--- backend/src/middleware/index.ts
+++ backend/src/middleware/index.ts
@@ -1,9 +1,10 @@
 import { logger, requestLogger } from "../logger";
 import bodyParser from "body-parser";
 import helmet from "helmet";
+import cors from 'cors';
 
 export default () => {
     logger.info("Registering common middleware...");
 
-    return [helmet(), bodyParser.json(), requestLogger];
+    return [helmet(), cors(), bodyParser.json(), requestLogger];
 };
