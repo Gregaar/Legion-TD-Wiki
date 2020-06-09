19b7477d710ac074d580e19d9ed2d75ed532ae99
G <gregfuk@gmail.com>
2020-06-09 23:36:43 +0100

    fix: move styled-components from root package.json to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,10 +22,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-// The AuthProvider is a function which checks the auth state by reaching out to the server, and returns and updates the current AuthContext.Provider value
-// depending on if the user has the correct cookies (refresh and/or access), the auth status is set to true or false
-// I must now create a protected route which uses the context of the Provider and renders conditionally depending on if they are authenticated or not.
-// Then, when users sign in, out, or register, the provider value must be updated to reflect the new authentication state.
-// I think i should create login, logout and register functions and pass them into the provider so they can alter the state when a user does one of these actions.
-// Additionally, on every render I should check if a user is authenticated or not (via cookies) and redirect where appropriate.
