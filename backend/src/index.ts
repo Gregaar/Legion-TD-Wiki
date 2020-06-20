82f6f964cf00f9aad358b2d07214fe165efdfe95
Gregaar <gregfuk@gmail.com>
2020-06-20 13:30:25 +0100

    heroku troubleshooting (#3)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,4 @@
-import config from "config";
-console.log(config.get("jwt.secret"));
-console.log(config.get("jwt.domain"));
-console.log(config.get("mongo.uri"));
-console.log(config.get("app.port"));                       
+import config from "config";                      
 import database from "./db/mongoose";
 import server from "./server";
 
