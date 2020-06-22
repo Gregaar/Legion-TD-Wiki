bc7208290916c1630e0b357a29d4468af79747cc
Gregaar <gregfuk@gmail.com>
2020-06-22 21:32:22 +0100

    change refreshSecret value to JWT_Rsecret

--- backend/config/production.ts
+++ backend/config/production.ts
@@ -7,7 +7,7 @@ export default {
   },
   jwt: {
     secret: process.env.JWT_Secret,
-    refreshSecret: process.env.JWT_Refresh_Secret,
+    refreshSecret: process.env.JWT_Rsecret,
     tokenLife: process.env.Token_Life,
     refreshTokenLife: process.env.Refresh_Token_Life,
     domain: process.env.My_Domain,
