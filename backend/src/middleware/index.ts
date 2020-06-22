e5aabedfca556ca9f8ab954abf60451902626aca
Gregaar <gregfuk@gmail.com>
2020-06-22 17:26:17 +0100

    add corsOptions

--- backend/src/middleware/index.ts
+++ backend/src/middleware/index.ts
@@ -5,8 +5,12 @@ import helmet from "helmet";
 
 import { logger, requestLogger } from "../logger";
 
+const corsOptions = {
+  credentials: true,
+};
+
 export default (): any[] => {
   logger.info("Registering common middleware...");
 
-  return [helmet(), cors(), bodyParser.json(), cookieParser(), requestLogger];
+  return [helmet(), cors(corsOptions), bodyParser.json(), cookieParser(), requestLogger];
 };
