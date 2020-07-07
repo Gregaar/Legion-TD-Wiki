9bfa672c0da1e94b19ab23d5a93780e15d1e39d5
Gregaar <gregfuk@gmail.com>
2020-07-07 01:02:38 +0100

    feat: add prophet random units in frontend and backend (#148)

--- backend/src/routes/legion/units.ts
+++ backend/src/routes/legion/units.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import {
+  prophetUnits,
   queriedUnits,
   searchByUnitBuilder,
   searchByUnitName,
@@ -14,6 +15,8 @@ router.get("/api/unit/name/:name", searchByUnitName);
 router.get("/api/unit/builder/:builder", searchByUnitBuilder);
 
 router.get("/api/unit/filters", queriedUnits);
+
+router.get("/api/unit/prophet", prophetUnits);
 
 export default (): Router => {
   logger.info("Registering 'Unit' routes...");
---
+++ frontend/src/shared/Interfaces/prophet-abilities-interface.tsx
@@ -0,0 +1,9 @@
+export default interface ProphetAbilitiesInterface {
+    Aura: number;
+    Buff: number;
+    Debuff: number;
+    Splash: number;
+    Heal: number;
+    Stun: number;
+    Summon: number;
+}
