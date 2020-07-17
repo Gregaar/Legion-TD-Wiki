ce2bddcca71e0246acc572324b4a17a306bcdcc7
Gregaar <gregfuk@gmail.com>
2020-07-17 23:59:19 +0100

    fix: remove duplicate NavLink

--- frontend/src/shared/Styles/shared-styles.tsx
+++ frontend/src/shared/Styles/shared-styles.tsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
 import Background from "../../assets/bg.png";
 import device from "./devices";
 
