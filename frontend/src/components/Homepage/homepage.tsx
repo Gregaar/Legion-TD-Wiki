f4e813123fa6e6250bdf36f243d9cf13fd6ca715
G <gregfuk@gmail.com>
2020-07-31 17:45:57 +0100

    refactor: add lazy loading to all routes

--- frontend/src/components/Homepage/homepage.tsx
+++ frontend/src/components/Homepage/homepage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import mapImage from "../../assets/homepage/map-image.png";
+import mapImage from "../../assets/homepage/map-image.jpg";
 import {
   A,
   HeadingTwo,
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,40 +1,60 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Route, Switch } from "react-router";
 
-import About from "./components/About/about";
-import Homepage from "./components/Homepage/homepage";
-import Secret from "./components/Secret/secret";
-import Builders from "./containers/Builders/builders";
-import BuilderUnits from "./containers/BuilderUnits/builder-units";
-import IndividualUnit from "./containers/IndividualUnit/individual-unit";
-import King from "./containers/King/king";
-import Login from "./containers/Login/login";
-import ResetPassword from "./containers/Login/ResetPassword/reset-password";
-import NoMatchPage from "./components/NoMatchPage/no-match-page";
-import Summons from "./containers/Summons/summons";
-import Units from "./containers/UnitSearch/units";
-import Waves from "./containers/Waves/waves";
-import ProtectedRoute from "./hoc/ProtectedRoute/protected-route";
+import Spinner from "./components/Spinner/spinner";
+
+const About = lazy(() => import("./components/About/about"));
+const Homepage = lazy(() => import("./components/Homepage/homepage"));
+const Secret = lazy(() => import("./components/Secret/secret"));
+const Builders = lazy(() => import("./containers/Builders/builders"));
+const BuilderUnits = lazy(() =>
+  import("./containers/BuilderUnits/builder-units")
+);
+const IndividualUnit = lazy(() =>
+  import("./containers/IndividualUnit/individual-unit")
+);
+const King = lazy(() => import("./containers/King/king"));
+const Login = lazy(() => import("./containers/Login/login"));
+const ResetPassword = lazy(() =>
+  import("./containers/Login/ResetPassword/reset-password")
+);
+const NoMatchPage = lazy(() =>
+  import("./components/NoMatchPage/no-match-page")
+);
+const Summons = lazy(() => import("./containers/Summons/summons"));
+const Units = lazy(() => import("./containers/UnitSearch/units"));
+const Waves = lazy(() => import("./containers/Waves/waves"));
+const ProtectedRoute = lazy(() =>
+  import("./hoc/ProtectedRoute/protected-route")
+);
 
 const Router: React.FC = () => {
   return (
-    <Switch>
-      <ProtectedRoute path="/secret" exact component={Secret} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/resetpassword/:id/:token" exact component={ResetPassword} />
-      <Route path="/units/:unitName" exact component={IndividualUnit} />
-      <Route path="/units" exact component={Units} />
-      <Route path="/builders/:builderName" exact component={BuilderUnits} />
-      <Route path="/builders" exact component={Builders} />
-      <Route path="/summons/:order" exact component={IndividualUnit} />
-      <Route path="/summons" exact component={Summons} />
-      <Route path="/waves/:level" exact component={IndividualUnit} />
-      <Route path="/waves" exact component={Waves} />
-      <Route path="/king" exact component={King} />
-      <Route path="/about" exact component={About} />
-      <Route path="/" exact component={Homepage} />
-      <Route path="*" component={NoMatchPage} />
-    </Switch>
+    <>
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <ProtectedRoute path="/secret" exact component={Secret} />
+          <Route path="/login" exact component={Login} />
+          <Route
+            path="/resetpassword/:id/:token"
+            exact
+            component={ResetPassword}
+          />
+          <Route path="/units/:unitName" exact component={IndividualUnit} />
+          <Route path="/units" exact component={Units} />
+          <Route path="/builders/:builderName" exact component={BuilderUnits} />
+          <Route path="/builders" exact component={Builders} />
+          <Route path="/summons/:order" exact component={IndividualUnit} />
+          <Route path="/summons" exact component={Summons} />
+          <Route path="/waves/:level" exact component={IndividualUnit} />
+          <Route path="/waves" exact component={Waves} />
+          <Route path="/king" exact component={King} />
+          <Route path="/about" exact component={About} />
+          <Route path="/" exact component={Homepage} />
+          <Route path="*" component={NoMatchPage} />
+        </Switch>
+      </Suspense>
+    </>
   );
 };
 
